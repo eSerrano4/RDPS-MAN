47cfa79cf0b4ed96360d5a563ff4208b89725b00
Eric <eric@lausd-mac-d2-f3-f7.local>
2017-04-23 17:35:43 -0700

    Initial Commit

---
+++ RDPS MAN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RDPS MAN
+//
+//  Created by Eric on 4/23/17.
+//  Copyright © 2017 Eric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
